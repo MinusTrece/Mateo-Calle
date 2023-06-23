d5765568db3b41dd89ac01cffbc169c3e2959f4f
eudematos <adictoalhentai8989@gmail.com>
2023-06-23 11:56:49 -0500

    first commit

--- Ejercicio 2/script.js
+++ Ejercicio 2/script.js
@@ -5,5 +5,11 @@ let isMarried;
 firstName = "Mateo ";//String
 age = 18;//number
 isMarried = false;//boolean
+if(isMarried==true){
+    console.log("Estoy casado")
+    }else{
+    console.log("No estoy casado")
+    }
+    ;
 
 console.log("Hola soy "+firstName+"y tengo "+age+" años");
